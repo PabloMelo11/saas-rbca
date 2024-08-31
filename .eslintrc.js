04c5a32b546c572e6f7a999e9208085e8e7b2ef8
PabloMelo <pablomelodev@gmail.com>
2024-08-31 14:00:28 -0300

    build: initial config

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  ignorePatterns: ["apps/**", "packages/**", "config/**"],
+  extends: ["@saas/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true
+  }  
+}
