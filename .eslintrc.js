f4b69112f371f9eb8e518ba9e7e6f70dbd5cd56d
PabloMelo <pablomelodev@gmail.com>
2024-08-31 16:32:46 -0300

    feat: package auth + tsconfig

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  ignorePatterns: ["apps/**", "packages/**", "config/**"],
-  extends: ["@saas/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true
-  }  
-}
